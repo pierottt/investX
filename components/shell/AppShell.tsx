189a0035e7564ae78c71c75829d2c2779ad1589e
pierottt <leephungthum@gmail.com>
2026-02-28 20:35:36 +0700

    fix pwa view bug

--- components/shell/AppShell.tsx
+++ components/shell/AppShell.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function AppShell({ children }: { children: React.ReactNode }) {
   return (
-    <div className="h-[100dvh] w-full overflow-x-hidden bg-app">
+    <div className="app-shell-height w-full overflow-x-hidden bg-app">
       <div className="mx-auto flex h-full w-full justify-center px-0 py-0 min-[360px]:px-2 min-[768px]:px-3">
         <div
           className="relative h-full w-full overflow-hidden rounded-none border border-white/5 min-[360px]:rounded-[24px] min-[390px]:rounded-[28px]"
